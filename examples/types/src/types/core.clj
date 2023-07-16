11247889eabf3028e932c6ea1595623827dfc091
Otávio Carmo <otavio.carmo2019@Outlook.com>
2023-07-16 12:32:17 -0300

    clojure types

---
+++ examples/types/src/types/core.clj
@@ -0,0 +1,13 @@
+(ns types.core)
+
+(type 10)
+(type 10.10)
+(type 10N)
+(type 10M)
+(type "string")
+(type true)
+(type (/ 10 3))
+(type [])
+(type nil)
+(type println)
+(type (defn soma [x y] (true)))
